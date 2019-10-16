5c76343ceb18382785c7ed9fc8886f2b48be4993
PeterRobinson4137 <peter.robinson4137@gmail.com>
2019-10-16 14:23:35 +0100

    added grid generation

--- generate_grid.f90
+++ generate_grid.f90
@@ -15,6 +15,15 @@
 ! linearly interpolated between these values for j between 1 and nj.
 ! i.e.  x(i,1) should be xlow(i), x(i,nj) should be xhigh(i), etc.
 
+      REAL, DIMENSION(1:ni,nj) :: x, y
+
+      do i=1,ni 
+            do j=0,nj-1
+                  x(i,j) = xlow(i)
+                  y(i,j) = ylow(i) + j*(yhigh(i)-ylow(i))/nj
+            end do
+      end do
+
 ! INSERT your code here
 
 ! Calculate the areas of the cells area(i,j)
@@ -24,7 +33,15 @@
 ! half of the cross product of the vectors forming the diagonals.
 ! see Hirsch volume 1, section 6.2.1. (or lecture).
 ! Make sure that the area comes out positive!
-
+      REAL, DIMENSION(1:ni-1,nj-1) :: area
+      do i=1,ni-1
+            do j=1,nj-1
+                  area(i,j) = 0.5*(((x(i+1,j+1)-x(i,j))*(y(i,j+1)-y(i+1,j)))-((y(i+1,j+1)-y(i,j))*(x(i,j+1)-x(i+1,j))))
+                  if(area(i,j).lt.0) then
+                        area(i,j) = -area(i,j)
+                  end if
+            end do
+      end do
 ! INSERT your code here
 
 ! Calculate the x and y components of the length vector of the i-faces
@@ -33,10 +50,41 @@
 ! magnitude equal to the length of the face.
 ! It is positive in the direction of an inward normal to the cell i,j .
 ! Call these lengths dlix(i,j) and dliy(i,j)
+      REAL, DIMENSION(1:ni-1,nj-1) :: dlix,dliy,dljx,dljy
+      REAL :: dmin
+      dmin = 1000
+      do i=1,ni-1
+            do j=1,nj-1
+                  dlix(i,j) = y(i,j+1)-y(i,j)
+
+                  if(dlix(i,j).lt.dmin) then
+                        dmin = dlix(i,j)
+                  end if
+
+                  dliy(i,j) = x(i,j+1)-x(i,j)
+
+                  if(dliy(i,j).lt.dmin) then
+                        dmin = dliy(i,j)
+                  end if
+
+                  dljx(i,j) = y(i+1,j)-y(i,j)
+
+                  if(dljx(i,j).lt.dmin) then
+                        dmin = dljx(i,j)
+                  end if
+
+                  dljy(i,j) = x(i+1,j)-x(i,j)
+
+                  if(dljy(i,j).lt.dmin) then
+                        dmin = dljy(i,j)
+                  end if
+
+            end do
+      end do
 
 ! INSERT your code here
 
-! Now calculcd ate the x and y components of the length vector of the j-faces. (i.e. those corresponding to j = constant)
+! Now calculate the x and y components of the length vector of the j-faces. (i.e. those corresponding to j = constant)
 ! Call these lengths dljx(i,j) and dljy(i,j)
 
 ! INSERT your code here
