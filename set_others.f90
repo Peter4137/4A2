961bfa42a064d63651d7f0487d7c7bc077b12a94
PeterRobinson4137 <peter.robinson4137@gmail.com>
2019-10-22 13:15:44 +0100

    added set_others calcs

--- set_others.f90
+++ set_others.f90
@@ -18,6 +18,15 @@
 ! the static pressure p(i,j) and the stagnation enthalpy hstag(i,j).
 ! Note:  "hstag"  not  "ho".
 
+      do i=1,ni
+            do j=1,nj
+                  vx(i,j) = rovx(i,j)/ro(i,j)
+                  vy(i,j) = rovy(i,j)/ro(i,j)
+                  p(i,j) = (gamma-1)*(roe(i,j)-0.5*ro(i,j)*norm2([vx(i,j),vy(i,j)]))
+                  hstag(i,j) = (roe(i,j)+p(i,j))/ro(i,j)
+            end do
+      end do
+      
 ! INSERT your code here
 
       end
